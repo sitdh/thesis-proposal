d14c1ace2940f1bd13e895a7840d2113d47e4a1f
Sitdhibong Laokok <sitdhibong@gmail.com>
2016-11-28 20:30:34 +0700

    Update code

--- related/SimpleBonusScore.java
+++ related/SimpleBonusScore.java
@@ -1,5 +1,7 @@
 class SimpleBonusScore {
+
     public int score(int student_score, int bonus_score) {
+
         if ( bonus_score > 0 ) {
             if ( student_score <= 50 ) {
                 student_score = Math.min(50, student_score + bonus_score);
@@ -7,6 +9,7 @@ class SimpleBonusScore {
                 student_score = Math.min(70, student_score + bonus_score);
             }
         }
+
         return student_score;
     }
 }
--- related/SimpleGrading.java
+++ related/SimpleGrading.java
@@ -1,21 +1,32 @@
 class SimpleGrading {
+
     protected SimpleBonusScore bonusScore;
+
+    protected static final int SCORE_MINIMUM_STATISFIED = 80;
+    protected static final int SCORE_INCOMPLETE_SCORE   = 0;
+    protected static final int SCORE_MAXIMUM            = 100;
+
     public static final String GRADE_LETTER_UNSATISFIED = "U";
     public static final String GRADE_LETTER_SATISFIED   = "S";
     public static final String GRADE_LETTER_INCOMPLETE  = "I";
+
     public SimpleGrading(SimpleBonusScore bonusScore) {
         this.bonusScore = bonusScore;
     }
+
     public String grading(int student_score, int bonus_score) {
+
         String grade_letter = "";
         student_score = this.bonusScore.score(student_score, bonus_score);
-        if (student_score < 80) {
+
+        if (student_score < SCORE_MINIMUM_STATISFIED) {
             grade_letter = SimpleGrading.GRADE_LETTER_UNSATISFIED;
-        } else if (student_score == 0) {
+        } else if (student_score == SCORE_MINIMUM_STATISFIED) {
             grade_letter = SimpleGrading.GRADE_LETTER_UNSATISFIED;
-        } else if (student_score == 100) {
+        } else if (student_score == SCORE_MINIMUM) {
             grade_letter = SimpleGrading.GRADE_LETTER_UNSATISFIED;
         }
+
         return grade_letter;
     }
 }
