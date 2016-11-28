07e6fe4258e8481c9906980f870265faf709dc7c
Sitdhibong Laokok <sitdhibong@gmail.com>
2016-11-28 15:12:23 +0700

    Split condition

--- related/SimpleBonusScore.java
+++ related/SimpleBonusScore.java
@@ -1,9 +1,11 @@
 class SimpleBonusScore {
     public int score(int student_score, int bonus_score) {
-        if (bonus_score > 0 && student_score <= 50) {
-            student_score = Math.min(50, student_score + bonus_score);
-        } else if (bonus_score > 0 && student_score <= 70) {
-            student_score = Math.min(70, student_score + bonus_score);
+        if ( bonus_score > 0 ) {
+            if ( student_score <= 50 ) {
+                student_score = Math.min(50, student_score + bonus_score);
+            } else if ( student_score <= 70 ) {
+                student_score = Math.min(70, student_score + bonus_score);
+            }
         }
         return student_score;
     }
