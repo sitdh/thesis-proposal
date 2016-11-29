f536c652c516b9e24838a9f2f20e3a5d5e74b939
Sitdhibong Laokok <sitdhibong@gmail.com>
2016-11-29 21:50:39 +0700

    Neary done

--- methodology/SimpleGradingTest-Refined.java
+++ methodology/SimpleGradingTest-Refined.java
@@ -2,9 +2,9 @@ public class SimpleGradingTest {
     @Test
     public void testSimpleGrading() {
         SimpleGrading g = new SimpleGrading(new SimpleBonusScore());
-        String grade_letter = g.grading("IUUUSISS", 65, 42);
+        String grade_letter = g.grading("5873000021", 65, 42);
 
-        assertEquals("lorem", grade_letter);
+        assertEquals("U", grade_letter);
     }
 
 }
