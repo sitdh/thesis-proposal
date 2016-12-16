fe4060a5e93b6a6fb90b4d139c88bd54bbc3cda9
Sitdhibong Laokok <sitdhibong@gmail.com>
2016-12-16 20:21:57 +0700

    Fine tune proposal

--- methodology/SimpleGradingTest-Refined.java
+++ methodology/SimpleGradingTest-Refined.java
@@ -2,7 +2,7 @@ public class SimpleGradingTest {
     @Test
     public void testSimpleGrading() {
         SimpleGrading g = new SimpleGrading(new SimpleBonusScore());
-        String grade_letter = g.grading("5873000021", 65, 42);
+        String grade_letter = g.grading("5873000021", 75, 0);
 
         assertEquals("U", grade_letter);
     }
